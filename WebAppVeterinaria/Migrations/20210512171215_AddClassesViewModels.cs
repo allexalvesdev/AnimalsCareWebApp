a7db0963ec30784f12aa0ea0ba9b27ee26b30ad5
Allex Alves <31211283+allexalvesdev@users.noreply.github.com>
2021-05-14 20:07:07 -0300

    Add files via upload

---
+++ WebAppVeterinaria/Migrations/20210512171215_AddClassesViewModels.cs
@@ -0,0 +1,327 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace WebAppVeterinaria.Migrations
+{
+    public partial class AddClassesViewModels : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Rg",
+                table: "Veterinarios",
+                type: "varchar(50)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "varchar(8)",
+                oldMaxLength: 8);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Numero",
+                table: "Veterinarios",
+                type: "varchar(50)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "varchar(15)",
+                oldMaxLength: 15);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "NomeCompleto",
+                table: "Veterinarios",
+                type: "varchar(100)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "varchar(100)",
+                oldMaxLength: 100);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Logradouro",
+                table: "Veterinarios",
+                type: "varchar(250)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "varchar(100)",
+                oldMaxLength: 100);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Estado",
+                table: "Veterinarios",
+                type: "varchar(20)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "varchar(50)",
+                oldMaxLength: 50);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Cpf",
+                table: "Veterinarios",
+                type: "varchar(20)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "varchar(11)",
+                oldMaxLength: 11);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "CodigoCRMV",
+                table: "Veterinarios",
+                type: "varchar(50)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "varchar(50)");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Cidade",
+                table: "Veterinarios",
+                type: "varchar(100)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "varchar(50)",
+                oldMaxLength: 50);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Cep",
+                table: "Veterinarios",
+                type: "varchar(20)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "varchar(8)",
+                oldMaxLength: 8);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Bairro",
+                table: "Veterinarios",
+                type: "varchar(100)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "varchar(100)",
+                oldMaxLength: 100);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Descricao",
+                table: "Servicos",
+                type: "varchar(500)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "varchar(500)",
+                oldMaxLength: 500);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Raca",
+                table: "Animais",
+                type: "varchar(50)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "varchar(50)",
+                oldMaxLength: 50);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Peso",
+                table: "Animais",
+                type: "varchar(50)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "varchar(50)");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Observacao",
+                table: "Animais",
+                type: "varchar(250)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "varchar(500)",
+                oldMaxLength: 500,
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Nome",
+                table: "Animais",
+                type: "varchar(50)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "varchar(200)",
+                oldMaxLength: 200);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Idade",
+                table: "Animais",
+                type: "varchar(50)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "varchar(50)");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Rg",
+                table: "Veterinarios",
+                type: "varchar(8)",
+                maxLength: 8,
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "varchar(50)",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Numero",
+                table: "Veterinarios",
+                type: "varchar(15)",
+                maxLength: 15,
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "varchar(50)",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "NomeCompleto",
+                table: "Veterinarios",
+                type: "varchar(100)",
+                maxLength: 100,
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "varchar(100)",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Logradouro",
+                table: "Veterinarios",
+                type: "varchar(100)",
+                maxLength: 100,
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "varchar(250)",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Estado",
+                table: "Veterinarios",
+                type: "varchar(50)",
+                maxLength: 50,
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "varchar(20)",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Cpf",
+                table: "Veterinarios",
+                type: "varchar(11)",
+                maxLength: 11,
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "varchar(20)",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "CodigoCRMV",
+                table: "Veterinarios",
+                type: "varchar(50)",
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "varchar(50)",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Cidade",
+                table: "Veterinarios",
+                type: "varchar(50)",
+                maxLength: 50,
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "varchar(100)",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Cep",
+                table: "Veterinarios",
+                type: "varchar(8)",
+                maxLength: 8,
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "varchar(20)",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Bairro",
+                table: "Veterinarios",
+                type: "varchar(100)",
+                maxLength: 100,
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "varchar(100)",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Descricao",
+                table: "Servicos",
+                type: "varchar(500)",
+                maxLength: 500,
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "varchar(500)",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Raca",
+                table: "Animais",
+                type: "varchar(50)",
+                maxLength: 50,
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "varchar(50)",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Peso",
+                table: "Animais",
+                type: "varchar(50)",
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "varchar(50)",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Observacao",
+                table: "Animais",
+                type: "varchar(500)",
+                maxLength: 500,
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "varchar(250)",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Nome",
+                table: "Animais",
+                type: "varchar(200)",
+                maxLength: 200,
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "varchar(50)",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Idade",
+                table: "Animais",
+                type: "varchar(50)",
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "varchar(50)",
+                oldNullable: true);
+        }
+    }
+}
