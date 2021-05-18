9bc2711441604da3d9f98d4f3e093c7797bae11c
Allex Alves <allexalves16@gmail.com>
2021-05-18 12:46:25 -0300

    Add correções cadastro
    Commit

--- WebAppVeterinaria/Startup.cs
+++ WebAppVeterinaria/Startup.cs
@@ -1,31 +1,22 @@
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Identity;
-using Microsoft.AspNetCore.Identity.UI;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using WebAppVeterinaria.Data;
-using WebAppVeterinaria.Entity;
 
 namespace WebAppVeterinaria
 {
     public class Startup
     {
-
-        public IConfiguration Configuration { get; }
-
         public Startup(IConfiguration configuration)
         {
             Configuration = configuration;
         }
 
+        public IConfiguration Configuration { get; }
 
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
